2c57c46631c5f4d53ac00752b98c6e87d6d83b6e
Ashley M. Kirchner <kirash4@gmail.com>
2017-07-07 23:22:29 -0600

    Added additional descriptions on config defaults section.

--- calendar_monthly.js
+++ calendar_monthly.js
@@ -9,14 +9,17 @@
 
 	// Module defaults
 	defaults: {
-		debugging:			false,
-		initialLoadDelay:	0,
-		fadeSpeed:			2,			// How fast to fade out and in during a midnight refresh
-		showHeader:			true,		// Show the month and year at the top of the calendar
-		cssStyle:			"block",	// which CSS style to use, 'clear', 'block', 'slate', or 'custom'
-		updateDelay:		5,			// How many seconds after midnight before a refresh
-										// This is to prevent collision with other modules refreshing
-										// at the same time.
+		debugging:		false,
+		initialLoadDelay:	0,		// How many seconds to wait on a fresh start up.
+							// This is to prevent collision with all other modules also
+							// loading all at the same time. This only happens once,
+							// when the mirror first starts up.
+		fadeSpeed:		2,		// How fast (in seconds) to fade out and in during a midnight refresh
+		showHeader:		true,		// Show the month and year at the top of the calendar
+		cssStyle:		"block",	// which CSS style to use, 'clear', 'block', 'slate', or 'custom'
+		updateDelay:		5,		// How many seconds after midnight before a refresh
+							// This is to prevent collision with other modules refreshing
+							// at the same time.
 	},
 
 	// Required styles
@@ -54,7 +57,7 @@
 
 		this.loaded = false;
 
-		this.scheduleUpdate(this.config.initialLoadDelay);
+		this.scheduleUpdate(this.config.initialLoadDelay * 1000);
 	},
 
 	// Override dom generator
